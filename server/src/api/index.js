074a5f59d372426c64be52a9f3a6e8d008d33b1a
Clément Levailant <clement@koji.fr>
2020-10-28 10:49:10 +0100

    Delete files in server

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 
-const emojis = require('./emojis');
 const items = require('./items');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({
-    message: 'API - 👋🌎🌍🌏'
+    message: 'API'
   });
 });
 
-router.use('/emojis', emojis);
 router.use('/items', items);
 
 module.exports = router;
